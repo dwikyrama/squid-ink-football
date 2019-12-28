8c8b4831755ec709cd207d7c6cd8e9f2e7b9ecad
dwikyrama <dwikyry@gmail.com>
2019-12-29 01:35:51 +0700

    Edited cache-part in api.js. Updated urlToCache. Cache is working except getTeamById (this part of code is for indexedDB).

--- js/api.js
+++ js/api.js
@@ -24,39 +24,47 @@ function error(error) {
 // Blok kode untuk mengambil data standing (ranking pertandingan)
 function getStandings() {
   if ('caches' in window) {
-    caches.match(base_url + "competitions/PL/standings").then(function (response) {
+    caches.match(base_url + "competitions/PL/standings?standingType=TOTAL").then(function (response) {
       if (response) {
         response.json().then(function (data) {
           var standingsHTML = "";
           data.standings[0].table.forEach(function (standing) {
             standingsHTML += `
-              <tr>
-                  <td>${standing.position}.</td>
-                  <td>${standing.team.name}</td>
-                  <td>${standing.playedGames}</td>
-                  <td>${standing.won}</td>
-                  <td>${standing.draw}</td>
-                  <td>${standing.lost}</td>
-                  <td>${standing.goalsFor}</td>
-                  <td>${standing.goalsAgainst}</td>
-                  <td>${standing.goalDifference}</td>
-                  <td>${standing.points}</td>
-              </tr>
+            <tr>
+                <td>${standing.position}</td>
+                <td>
+                  <img class="standing-img" src="${standing.team.crestUrl}" />
+                </td>
+                <td class="align-left-td">
+                  <a href="./team_info.html?team_id=${standing.team.id}">
+                    ${standing.team.name}
+                  </a>
+                </td>
+                <td>${standing.playedGames}</td>
+                <td>${standing.won}</td>
+                <td>${standing.draw}</td>
+                <td>${standing.lost}</td>
+                <td>${standing.goalsFor}</td>
+                <td>${standing.goalsAgainst}</td>
+                <td>${standing.goalDifference}</td>
+                <td>${standing.points}</td>
+            </tr>
             `;
           });
-          // Sisipkan komponen card ke dalam elemen dengan id #content
+          // Sisipkan komponen tabel ke dalam elemen dengan id #standings
           document.getElementById("standings").innerHTML = standingsHTML;
         })
       }
     })
   }
+
   fetch(base_url + "competitions/PL/standings?standingType=TOTAL", {
     headers:{'X-Auth-Token': '91edc29ed6324ae0b36c5b14383062d0'}
   })
     .then(status)
     .then(json)
     .then(function (data) {
-      // Blok kode untuk menampilkan tabel standing
+      // Blok kode untuk menyusun komponen tabel standing
       var standingsHTML = "";
       data.standings[0].table.forEach(function (standing) {
           standingsHTML += `
@@ -81,7 +89,7 @@ function getStandings() {
             </tr>
             `;
       });
-      // Sisipkan komponen card ke dalam elemen dengan id #content
+      // Sisipkan komponen tabel ke dalam elemen dengan id #standings
       document.getElementById("standings").innerHTML = standingsHTML;
     })
     .catch(error);
@@ -94,21 +102,22 @@ function getTeams() {
       if (response) {
         response.json().then(function (data) {
           var teamsHTML = "";
-          data.teams[0].table.forEach(function (team) {
+          data.teams.forEach(function (team) {
             teamsHTML += `
-              <tr>
-                  <td>${team.position}.</td>
-                  <td>${team.team.name}</td>
-                  <td>${team.playedGames}</td>
-                  <td>${team.won}</td>
-                  <td>${team.draw}</td>
-                  <td>${team.lost}</td>
-                  <td>${team.goalsFor}</td>
-                  <td>${team.goalsAgainst}</td>
-                  <td>${team.goalDifference}</td>
-                  <td>${team.points}</td>
-              </tr>
-            `;
+              <div class="col s12 m6 l4">
+                <div class="card">
+                    <a href="./team_info.html?team_id=${team.id}">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="${team.crestUrl}">
+                    </div>
+                    </a>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">${team.name}</span>
+                        <p>Founded: ${team.founded}</p>
+                    </div>
+                </div>  
+              </div>  
+              `;
           });
           // Sisipkan komponen card ke dalam elemen dengan id #content
           document.getElementById("teams").innerHTML = teamsHTML;
@@ -150,6 +159,43 @@ function getTeams() {
 
 // Blok kode untuk mengambil data jadwal pertandingan
 function getMatchday() {
+  if ('caches' in window) {
+    caches.match(base_url + "competitions/PL").then(function (response) {
+      if (response) {
+        response.json().then(function (data) {
+          caches.match(base_url + "competitions/PL/matches?matchday=" + data.currentSeason.currentMatchday).then(function (response) {
+            if (response) {
+              response.json().then(function (data) {
+                var matchesHTML = "";
+                data.matches.forEach(function (match) {
+                  var score;
+                  if (match.score.fullTime.homeTeam) {
+                    score = match.score.fullTime.homeTeam + ":" + match.score.fullTime.awayTeam;
+                  } else {
+                    score = "-:-";
+                  }
+
+                  matchesHTML += `
+                      <tr>
+                        <td>${match.matchday}</td>
+                        <td>${match.utcDate}</td>
+                        <td>${match.status}</td>
+                        <td>${match.homeTeam.name}</td>
+                        <td>${score}</td>
+                        <td>${match.awayTeam.name}</td>
+                      </tr>
+                  `;
+                });
+                // Sisipkan komponen tabel ke dalam elemen dengan id #content
+                document.getElementById("matches").innerHTML = matchesHTML;
+              })
+            }
+          })
+        })
+      }
+    })
+  }
+
   fetch(base_url + "competitions/PL", {
     headers: { 'X-Auth-Token': '91edc29ed6324ae0b36c5b14383062d0' }
   })
@@ -163,7 +209,7 @@ function getMatchday() {
         .then(json)
         .then(function (data) {
           // Objek/array JavaScript dari response.json() masuk lewat data.
-    
+
           var matchesHTML = "";
           data.matches.forEach(function (match) {
             var score;
@@ -172,7 +218,7 @@ function getMatchday() {
             } else {
               score = "-:-";
             }
-    
+
             matchesHTML += `
                 <tr>
                   <td>${match.matchday}</td>
@@ -188,7 +234,7 @@ function getMatchday() {
           document.getElementById("matches").innerHTML = matchesHTML;
         })
         .catch(error);
-   }).catch(error);
+    }).catch(error);
 }
 
 // Blok kode untuk menambahkan opsi matchday dalam dropdown
@@ -202,36 +248,39 @@ function getMatchdayDropdown() {
 }
 
 function getMatchesByDay() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var idParam = urlParams.get("matchday");
+
   if ('caches' in window) {
-    caches.match(base_url + "competitions/PL/matches").then(function (response) {
+    caches.match(base_url + "competitions/PL/matches?matchday=" + idParam).then(function (response) {
       if (response) {
         response.json().then(function (data) {
-          var teamsHTML = "";
-          data.teams[0].table.forEach(function (team) {
-            teamsHTML += `
-              <tr>
-                  <td>${team.position}.</td>
-                  <td>${team.team.name}</td>
-                  <td>${team.playedGames}</td>
-                  <td>${team.won}</td>
-                  <td>${team.draw}</td>
-                  <td>${team.lost}</td>
-                  <td>${team.goalsFor}</td>
-                  <td>${team.goalsAgainst}</td>
-                  <td>${team.goalDifference}</td>
-                  <td>${team.points}</td>
-              </tr>
+          var matchesHTML = "";
+          data.matches.forEach(function (match) {
+            var score;
+            if (match.score.fullTime.homeTeam) {
+              score = match.score.fullTime.homeTeam + ":" + match.score.fullTime.awayTeam;
+            } else {
+              score = "-:-";
+            }
+
+            matchesHTML += `
+            <tr>
+              <td>${match.matchday}</td>
+              <td>${match.utcDate}</td>
+              <td>${match.status}</td>
+              <td>${match.homeTeam.name}</td>
+              <td>${score}</td>
+              <td>${match.awayTeam.name}</td>
+            </tr>
             `;
           });
-          // Sisipkan komponen card ke dalam elemen dengan id #content
-          document.getElementById("teams").innerHTML = teamsHTML;
+          // Sisipkan komponen tabel ke dalam elemen dengan id #content
+          document.getElementById("matches").innerHTML = matchesHTML;
         })
       }
     })
   }
-
-  var urlParams = new URLSearchParams(window.location.search);
-  var idParam = urlParams.get("matchday");
 
   fetch(base_url + "competitions/PL/matches?matchday=" + idParam, {
     headers: { 'X-Auth-Token': '91edc29ed6324ae0b36c5b14383062d0' }
@@ -239,8 +288,6 @@ function getMatchesByDay() {
     .then(status)
     .then(json)
     .then(function (data) {
-      // Objek/array JavaScript dari response.json() masuk lewat data.
-
       var matchesHTML = "";
       data.matches.forEach(function (match) {
         var score;
@@ -272,22 +319,69 @@ function getTeamById() {
     // Ambil nilai query parameter (?id=)
     var urlParams = new URLSearchParams(window.location.search);
     var idParam = urlParams.get("team_id");
+
     if ("caches" in window) {
-      caches.match(base_url + "article/" + idParam).then(function (response) {
+      caches.match(base_url + "teams/" + idParam).then(function (response) {
         if (response) {
           response.json().then(function (data) {
-            var articleHTML = `
-                <div class="card">
-                  <div class="card-image waves-effect waves-block waves-light">
-                    <img src="${data.result.cover}" />
-                  </div>
-                  <div class="card-content">
-                    <span class="card-title">${data.result.post_title}</span>
-                    ${snarkdown(data.result.post_content)}
-                  </div>
-                </div>
-              `;
-            // Sisipkan komponen card ke dalam elemen dengan id #content
+            var teamHTML = `
+          <h3>${data.name}</h3>
+          <div class="row">
+            <div class="col s12 m4 l3">
+              <img class="team-info-img" src="${data.crestUrl}" />
+            </div>
+            <div class="col s12 m8 l9">
+                <ul class="collection">
+                    <li class="collection-item">Founded <b>${data.founded}</b></li>
+                    <li class="collection-item">Phone <b>${data.phone}</b></li>
+                    <li class="collection-item">Email <b>${data.email}</b></li>
+                    <li class="collection-item">Website <b>${data.website}</b></li>
+                    <li class="collection-item">Address <b>${data.address}</b></li>
+                </ul>
+            </div>
+          </div>       
+        `;
+
+            var playerHTML = "";
+            data.squad.forEach(function (player) {
+              var shirtNumber = "";
+              if (player.shirtNumber) {
+                shirtNumber = player.shirtNumber;
+              }
+
+              if (player.role == "PLAYER") {
+                playerHTML += `
+              <tr>
+                <td>${player.name}</td>
+                <td>${player.position}</td>
+                <td>${shirtNumber}</td>
+                <td>${player.dateOfBirth}</td>
+                <td>${player.nationality}</td>
+              </tr>
+            `
+              }
+            });
+
+            teamHTML += `
+        <h5>Players</h5>
+        <hr />  
+        <table class="highlight">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Shirt Number</th>
+                <th>DOB</th>
+                <th>Nationality</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${playerHTML}
+            <tbody>
+          </table>
+          <br />
+        `;
+            // Sisipkan komponen tabel ke dalam elemen dengan id #body-content
             document.getElementById("body-content").innerHTML = articleHTML;
             // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
             resolve(data);
@@ -295,15 +389,13 @@ function getTeamById() {
         }
       });
     }
+
     fetch(base_url + "teams/" + idParam, {
-      headers:{'X-Auth-Token': '91edc29ed6324ae0b36c5b14383062d0'}
+      headers: { 'X-Auth-Token': '91edc29ed6324ae0b36c5b14383062d0' }
     })
       .then(status)
       .then(json)
       .then(function (data) {
-        // Objek JavaScript dari response.json() masuk lewat variabel data.
-        console.log(data);
-        // Menyusun komponen card artikel secara dinamis
         var teamHTML = `
           <h3>${data.name}</h3>
           <div class="row">
@@ -321,21 +413,15 @@ function getTeamById() {
             </div>
           </div>       
         `;
-        
-        // data.activeCompetitions.forEach(function (activeComp) {
-        //   teamHTML += `
-        //     <p>Competition ${activeComp.name}</p>            
-        //   `;
-        // })
 
         var playerHTML = "";
         data.squad.forEach(function (player) {
           var shirtNumber = "";
-          if(player.shirtNumber) {
+          if (player.shirtNumber) {
             shirtNumber = player.shirtNumber;
           }
-        
-          if(player.role == "PLAYER") {
+
+          if (player.role == "PLAYER") {
             playerHTML += `
               <tr>
                 <td>${player.name}</td>
@@ -368,7 +454,7 @@ function getTeamById() {
           <br />
         `;
 
-        // Sisipkan komponen card ke dalam elemen dengan id #content
+        // Sisipkan komponen tabel ke dalam elemen dengan id #body.content
         document.getElementById("body-content").innerHTML = teamHTML;
         // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
         resolve(data);
